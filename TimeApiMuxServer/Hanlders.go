d20178d9f4a3e017e8e77aefbc74a05f64f62965
Shivam Gupta <shivamg@zeta.tech>
2022-02-12 14:02:40 +0530

    rewriting time server for understandable code

--- TimeApiMuxServer/Hanlders.go
+++ TimeApiMuxServer/Hanlders.go
@@ -7,33 +7,48 @@ import (
 )
 
 func BaseHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
 
-	if q := r.URL.Query(); len(q) == 0 {
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(200) //This needs to come last
-		json.NewEncoder(w).Encode(map[string]string{"current_time": time.Now().String()})
+	if len(q) == 0 {
+		ContHeaderAndResCode(w, "application/json", 200)
+		JsonEncodeData(w, map[string]string{"current_time": time.Now().String()})
 	} else {
-		v, ok := q["tz"]
-		if !ok {
-			w.Header().Add("Content-Type", "application/json")
-			w.WriteHeader(404)
+		parseQueries(w, q)
+	}
+}
+
+func parseQueries(w http.ResponseWriter, q map[string][]string) {
+	v, ok := q["tz"]
+
+	if !ok {
+		ContHeaderAndResCode(w, "application/json", 404)
+	} else {
+		parseAndLoadTzs(w, q, v)
+	}
+}
+
+func parseAndLoadTzs(w http.ResponseWriter, q map[string][]string, v []string) {
+	times := make(map[string]string, len(v))
+
+	for _, s := range v {
+		loc, err := time.LoadLocation(s)
+		if err != nil {
+			times[s] = "tz not found"
 		} else {
-			times := make(map[string]string, len(v))
-
-			for _, s := range v {
-				loc, err := time.LoadLocation(s)
-				if err != nil {
-					times[s] = "tz not found"
-				} else {
-					times[s] = time.Now().In(loc).String()
-				}
-			}
-
-			w.Header().Add("Content-Type", "application/json")
-			w.WriteHeader(200) //This needs to come last
-			json.NewEncoder(w).Encode(times)
-
+			times[s] = time.Now().In(loc).String()
 		}
 	}
 
+	ContHeaderAndResCode(w, "application/json", 200)
+	JsonEncodeData(w, times)
+
+}
+
+func JsonEncodeData(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(data)
+}
+
+func ContHeaderAndResCode(w http.ResponseWriter, value string, resCode int) {
+	w.Header().Add("Content-Type", value)
+	w.WriteHeader(resCode) //This needs to come last
 }
